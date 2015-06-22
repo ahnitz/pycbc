1638824314eb510c38e0d92dc614f318d2f324ad
Alexander Harvey Nitz <alex.nitz@gmail.com>
2015-06-22 16:59:31 -0400

    simplify travis

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -6,19 +6,14 @@ mkdir -p $LOCAL/src
 
 sudo apt-get install \
 libfftw3-dev \
-python-decorator \
-python-jinja2 \
-python-h5py \
 libhdf5-serial-dev \
 liblapack-dev \
-python-scipy \
-python-numpy \
 gfortran \
 libgsl0-dev
 
-pip install decorator --upgrade
-pip install matplotlib --upgrade
-pip install cython --upgrade
+pip install numpy >= 1.6.4
+pip install cython
+SWIG_FEATURES="-cpperraswarn -includeall -I/usr/include/openssl" pip install M2Crypto
 
 # install the version of swig that for some reason we have to use
 wget http://downloads.sourceforge.net/project/swig/swig/swig-2.0.11/swig-2.0.11.tar.gz
@@ -29,12 +24,6 @@ export LD_LIBRARY_PATH=$LOCAL/lib:$LOCAL/lib64
 export PKG_CONFIG_PATH=$LOCAL/lib/pkgconfig
 export PATH=$PATH:$LOCAL/bin
 
-
-# Standard python dependencies
-pip install pillow 
-pip install -e 'git+http://github.com/jakevdp/mpld3.git#egg=mpld3-0.3'
-SWIG_FEATURES="-cpperraswarn -includeall -I/usr/include/openssl" pip install M2Crypto
-
 # Install metaio
 wget https://www.lsc-group.phys.uwm.edu/daswg/download/software/source/metaio-8.2.tar.gz
 tar -zxvf metaio-8.2.tar.gz
@@ -44,7 +33,6 @@ cd metaio-8.2; CPPFLAGS=-std=gnu99 ./configure --prefix=$LOCAL; make -j; make in
 wget http://lappweb.in2p3.fr/virgo/FrameL/v8r26.tar.gz
 tar -xvf v8r26.tar.gz 
 cd v8r26; autoreconf; ./configure --prefix=$LOCAL;make -j; make install; cd ..
-
 
 # Install lalsuite itself
 cd $LOCAL/src/
