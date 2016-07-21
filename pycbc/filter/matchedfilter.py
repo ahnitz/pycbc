1a57e4787d39a31c6924cb2abf4bbe3e59732357
Alexander Harvey Nitz <ahnitz@syr.edu>
2016-07-21 21:20:36 +0200

    return nothing

--- pycbc/filter/matchedfilter.py
+++ pycbc/filter/matchedfilter.py
@@ -1430,7 +1430,7 @@ class LiveBatchMatchedFilter(object):
             if self.snr_abort_threshold is not None and s > self.snr_abort_threshold:
                 logging.info("We are seeing some *really* high SNRs, lets"
                              "assume they aren't signals and just give up")
-                return False, _
+                return False, []
      
             snrv = numpy.array([htilde.out[l]])
             veto_info.append((snrv, norm, l, htilde, stilde))
