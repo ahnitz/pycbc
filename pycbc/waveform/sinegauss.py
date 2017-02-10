999bdc6ab294c653a2fc1d78a0cd4b472eeb2fa5
Alexander Harvey Nitz <alex.nitz@ligo.org>
2017-02-10 16:03:00 +0100

    fix sinegaussian generator

--- pycbc/waveform/sinegauss.py
+++ pycbc/waveform/sinegauss.py
@@ -4,5 +4,9 @@ import numpy
 def fd_sine_gaussian(amp, quality, central_frequency, fmin, fmax, delta_f):
     f = numpy.arange(0, fmax, delta_f)
     tau = quality / 2 / numpy.pi / central_frequency
-    d = amp * numpy.pi ** 0.5 / 2 * tau * numpy.exp(-numpy.pi ** 2.0 * tau * tau * (f - central_frequency) * (1 + numpy.exp(-quality ** 2.0 * f / central_frequency))
-    return pycbc.types.FrequencySeries(d, delta_f = delta_f)
+    d = amp * numpy.pi ** 0.5 / 2 * tau 
+    d *= numpy.exp(-(numpy.pi  * tau  * (f - central_frequency))**2.0)
+    d *= (1 + numpy.exp(-quality ** 2.0 * f / central_frequency))
+    d[0:int(fmin / delta_f)] = 0
+    return pycbc.types.FrequencySeries(d.astype(numpy.complex128), delta_f = delta_f)
+
