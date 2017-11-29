3c38c8761d147672e3beef877ef9c38c5dbfdd04
Alexander Harvey Nitz <alex.nitz@ligo.org>
2017-11-29 14:45:09 +0100

    allow mb

--- pycbc/detector.py
+++ pycbc/detector.py
@@ -36,12 +36,14 @@ class Detector(object):
     """A gravitaional wave detector
     """
     def __init__(self, detector_name):
+        self.name = str(detector_name)
 
         # Allow for duplicate detectors
         if 'DUPLICATE' in detector_name:
             self.real_name = detector_name[0:2]
-            
-        self.name = str(detector_name)
+        else:
+            self.real_name = self.name
+
         self.frDetector =  lalsimulation.DetectorPrefixToLALDetector(self.real_name)
         self.response = self.frDetector.response
         self.location = self.frDetector.location
